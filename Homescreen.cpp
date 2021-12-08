f4587c0b9e396f6af2589047af385e2ec6b39b89
Sathiya Naraayanan <77498786+born2win685@users.noreply.github.com>
2021-12-08 12:31:55 +0530

    Update Homescreen.cpp

--- Homescreen.cpp
+++ Homescreen.cpp
@@ -1,20 +1,36 @@
 #include "Homescreen.h"
 
 void Window::makeWindow(const char* title, int width, int height)
-:window(NULL),renderer(NULL)
 {
-    if SDL_Init(!(SDL_INIT_EVERYTHING))
+    if(SDL_Init(SDL_INIT_EVERYTHING)!=0)
     {
-        window = SDL_CreateWindow("title",SDL_WINDOWPOS_CENTERED_DISPLAY , SDL_WINDOWPOS_CENTERED_DISPLAY ,width,height,SDL_WINDOW_SHOWN);
-        if(window==NULL)
+        cout<<"error: Failed to initialize "<< SDL_GetError()<<endl;    
+    }
+    window = SDL_CreateWindow("Ammomatics v1.0",0 , 0 ,width,height,0);
+    if(window==NULL)
             cout<<"Couldn't create window.   error"<< SDL_GetError() << endl;
-        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-		if (renderer==NULL)
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer==NULL)
 		{
 			cout <<"Couldn't create renderer. error"<< SDL_GetError() << endl;
 		}
+
+    surface=SDL_GetWindowSurface(window);
+    bg_image=SDL_LoadBMP("/home/born2win685/Desktop/cpp_project/images/bg.bmp");
+    SDL_BlitSurface(bg_image, NULL,surface,NULL);
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event event;
+    bool quit=true;
+    while(quit)
+    {
+        while(SDL_PollEvent(&event)!=0)
+        {
+
+        if(event.type == SDL_QUIT)
+            quit=false;
+        }
     }
-    return 0;
 }
 
 Window::~Window()
